20386e92347b8ad2fd77013fc2c96507588c6f29
Ynda Jas <yndajas@gmail.com>
2021-03-24 15:37:40 +0000

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(array) {
+  const season = array.find(season => season.result === "W");
+  if (season) {
+    return season.year;
+  }
+}
